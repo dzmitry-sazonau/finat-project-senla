d38c547035e80adc107258ba2cfe2cf8a0057c6d
Dima Sazonov <mister.mitya99@gmail.com>
2019-05-11 21:59:44 +0300

    added pagination

--- src/components/PhoneList.js
+++ src/components/PhoneList.js
@@ -10,15 +10,48 @@ class PhoneList extends Component {
     phones: PropTypes.array.isRequired,
   }
 
+  state = {
+    currentPage: 1,
+    todosPerPage: 15
+  }
+
+  handleClick = (event) => {
+    this.setState({currentPage: Number(event.target.id)});
+  }
+
   render() {
     const {phones} = this.props;
+    const {currentPage, todosPerPage} = this.state;
+
+    // Logic for displaying current todos
+    const indexOfLastTodo = currentPage * todosPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    const currentTodos = phones.slice(indexOfFirstTodo, indexOfLastTodo);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(phones.length / todosPerPage); i += 1) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => (
+      <li
+        role="presentation"
+        key={number}
+        id={number}
+        onClick={this.handleClick}
+      >
+        {number}
+      </li>
+    ));
 
     return (
       <div className="list-phones">
-        <Dropdown op={this.selectedPhones} />
-        {phones.map(item => (
+        <Dropdown />
+        {currentTodos.map(item => (
           <PhoneItem key={item.id} {...item} />
         ))}
+        {renderPageNumbers}
       </div>
     );
   }
